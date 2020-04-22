b4a5716aa6e1b65f324108f7af8398884df2cf19
cheese-bit <63467622+cheese-bit@users.noreply.github.com>
2020-04-22 06:14:32 -0700

    Add files via upload

---
+++ lemonade stand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand_3DayStarter
+{
+    class Player
+    {
+        // member variables (HAS A)
+        public Inventory inventory;
+        public Wallet wallet;
+        public string name;
+        public List<string> names;
+        // constructor (SPAWNER)
+        public Player()
+        {
+            inventory = new Inventory();
+            wallet = new Wallet();
+            names = new List<string>() { "Brad", "Cheeseman", "Roosevelt" };
+            name = ChooseName();
+        }
+
+
+
+
+
+        // member methods (CAN DO)
+        public string ChooseName()
+        {
+          Random randObj = new Random();
+          int randomNumber = randObj.Next(names.Count - 1);
+          return names[randomNumber];
+          
+        }
+   
+    }
+        
+        
+      
+}
